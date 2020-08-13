c92fb4a782733a95fa84303802f52b906b2be49c
Alex Chi <iskyzh@gmail.com>
2020-08-13 13:13:36 +0800

    add rustup

--- julia/test.sh
+++ julia/test.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+julia -e 'using Pkg; pkg"add Images DifferentialEquations Plots Flux JuMP Makie"'
 julia -e 'using Pkg; Pkg.add("Images")'
 julia -e 'using Pkg; Pkg.add("Plots")'
 julia -e 'using Pkg; Pkg.add("FFMPEG")'
@@ -11,4 +12,3 @@ julia -e 'using Pkg; Pkg.add("Turing")'
 julia -e 'using Pkg; Pkg.add("Flux")'
 julia -e 'using Pkg; Pkg.add("IJulia")'
 julia -e 'using Pkg; Pkg.add("Conda")'
-julia -e 'using Conda; Conda.channels()'
