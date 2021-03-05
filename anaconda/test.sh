b3525000052935dbe126bc923cae8a543da91a94
Alex Chi <iskyzh@gmail.com>
2021-03-05 12:27:31 +0800

    voidlinux: initial commit

--- anaconda/test.sh
+++ anaconda/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 set -e
-
-conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
 conda update conda
 conda create -n tensorflow tensorflow
 conda create -c pytorch -n pytorch pytorch
--- ubuntu/test.sh
+++ ubuntu/test.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-apt-get update -y -qq
+apt-get update -y 
 apt-get install -y wget git --download-only
